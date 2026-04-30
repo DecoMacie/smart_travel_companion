f74686e38a1647615bc14ad5759609a7219c7f10
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2026-05-01 00:45:36 +0100

    Vercel test

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -14,6 +14,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+
 const app = initializeApp(firebaseConfig);
 
 // Export Firebase services
